51fc978669512b2a5bdd15539ce6c54d8d64225f
Boris Gribkov <boris.gribkov@devol.network>
2024-04-02 12:41:50 +0400

    Add trade log account

--- src/accounts/log_accounts/mod.rs
+++ src/accounts/log_accounts/mod.rs
@@ -3,3 +3,4 @@ pub mod payoff_log;
 pub mod lp_trade_log;
 pub mod pools_log;
 pub mod task_log;
+pub mod trade_log;
--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,5 @@
 pub const BOUNDS_COUNT: usize = 94;
 pub const BUCKETS_COUNT: usize = 95;
+
+pub const VANILLA_COST_SIZE: usize = 4;
+pub const VANILLA_MEMO_SIZE: usize = 49;
