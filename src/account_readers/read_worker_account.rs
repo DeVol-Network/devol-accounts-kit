5800af66779d43692db31a655dd6af2804e617ed
Boris Gribkov <boris.gribkov@devol.network>
2024-04-09 16:51:17 +0400

    Update worker account

--- src/account_readers/read_worker_account.rs
+++ src/account_readers/read_worker_account.rs
@@ -30,57 +30,20 @@ mod tests {
     use crate::tests::tests::setup_account_reader;
 
     #[test]
-    fn test_read_root_account() {
+    fn test_read_worker_account() {
         let reader = setup_account_reader();
-        let worker_0 = reader.read_indexed::<WorkerAccount>(0, None).unwrap();
+        // Test read by index
+        let worker_0 = reader.read_indexed::<WorkerAccount>(0,None).unwrap();
+        check_worker_account(&worker_0);
+        // Test read by public key
+        let mints_account = reader.read::<AllWorkersAccount>(None).unwrap();
+        let pubkey = &mints_account.workers[0].address;
+        let worker_0 = reader.read_by_public_key::<WorkerAccount>(pubkey,None).unwrap();
+        check_worker_account(&worker_0);
+    }
 
-        assert_eq!(worker_0.header.tag, WORKER_ACCOUNT_TAG as u32);
-        assert_eq!(worker_0.header.version, WORKER_ACCOUNT_VERSION);
+    fn check_worker_account(mint_log_account: &WorkerAccount){
+        assert_eq!(mint_log_account.header.tag, WORKER_ACCOUNT_TAG as u32);
+        assert_eq!(mint_log_account.header.version, WORKER_ACCOUNT_VERSION);
     }
 }
-
-
-// use std::error::Error;
-// use solana_client::rpc_client::RpcClient;
-// use solana_sdk::account::ReadableAccount;
-// use crate::account_readers::read_all_workers_account::read_all_workers_account;
-// use crate::accounts::worker::worker_account::WorkerAccount;
-// use crate::data_structure::{WORKER_ACCOUNT_SIZE};
-//
-// pub fn read_worker_account(client: &RpcClient, id: usize) -> Result<WorkerAccount, Box<dyn Error>> {
-//     let all_workers = read_all_workers_account(&client)?;
-//     let key = all_workers.workers[id].address;
-//     let rpc_data = client.get_account(&key)?;
-//     if rpc_data.data.len() != WORKER_ACCOUNT_SIZE {
-//         return Err("Incorrect worker account size".into());
-//     }
-//     Ok(WorkerAccount::from(rpc_data.data()))
-// }
-//
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use solana_client::rpc_client::RpcClient;
-//     use crate::data_structure::{WORKER_ACCOUNT_TAG, WORKER_ACCOUNT_VERSION};
-//     use crate::env::RPC_URL;
-//
-//     #[test]
-//     fn test_read_worker_account() {
-//         let client = RpcClient::new(String::from(RPC_URL));
-//
-//         let worker_id = 0;
-//
-//         let worker_account_result = read_worker_account(&client, worker_id);
-//
-//         if let Err(e) = &worker_account_result {
-//             println!("Error: {}", e);
-//         }
-//
-//         assert!(worker_account_result.is_ok(), "The result should be ok but was Err");
-//
-//         let worker_account = worker_account_result.expect("Expected Ok value, got Err");
-//
-//         assert_eq!(worker_account.header.tag, WORKER_ACCOUNT_TAG as u32, "Worker account tag mismatch");
-//         assert_eq!(worker_account.header.version, WORKER_ACCOUNT_VERSION as u32, "Worker account version mismatch");
-//     }
-// }
