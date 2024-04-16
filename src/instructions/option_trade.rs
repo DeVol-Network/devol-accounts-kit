303ea3a120e1fb4bb3d6d497d6e71f7ac0589eed
Leonid Kabanov <Leonid.Kabanov@devol.network>
2024-04-16 19:57:09 +0200

    More tests for Option Trade

--- src/instructions/option_trade.rs
+++ src/instructions/option_trade.rs
@@ -4,7 +4,7 @@ use crate::dvl_off_chain_error::DvlOffChainError;
 use crate::instructions::devol_instruction_data::DevolInstructionData;
 use crate::instructions::instructions::Instructions;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialOrd, PartialEq, Debug)]
 #[repr(C)]
 struct BasketData {
     strike: u32,      //  4 bytes, offset: 0
@@ -54,13 +54,14 @@ impl<'a> DevolInstructionData<'a> for InstructionOptionTrade {
             reserved: 0,
             basket_length: basket_length as u8,
             trade_qty: params.trade_qty,
-            max_cost: params.max_cost.unwrap_or(-1000000000),
+            max_cost: params.max_cost.unwrap_or(DEFAULT_MAX_COST),
             basket: basket_array,
         }))
     }
 }
 
 const INSTRUCTION_VERSION: u8 = 2;
+const DEFAULT_MAX_COST: i64 = -1_000_000_000;
 
 #[cfg(test)]
 impl Default for InstructionOptionTrade{
@@ -115,7 +116,54 @@ mod tests {
         assert_eq!(&data.basket as *const _ as usize - base_ptr, INSTR_OPTION_TRADE_BASKET_DATA_OFFSET);
 
         assert_eq!(mem::size_of::<InstructionOptionTrade>(), INSTRUCTION_OPTION_TRADE_DATA_SIZE);
+    }
+
+    #[test]
+    fn test_default_instruction_option_trade() {
+        let trade_params = OptionTradeParams {
+            trade_qty: [0; BUCKETS_COUNT],
+            basket: None,
+            max_cost: None,
+        };
+        let data = DvlInstruction::new::<InstructionOptionTrade>(trade_params).unwrap();
         assert_eq!(data.cmd, Instructions::OptionTrade as u8);
         assert_eq!(data.version, INSTRUCTION_VERSION);
+        assert_eq!(data.max_cost, DEFAULT_MAX_COST);
+    }
+
+    #[test]
+    fn test_filled_basket_instruction_option_trade() {
+        let custom_basket_data = [
+            BasketData { strike: 10, pc: 20, amount: -30 },
+            BasketData { strike: 40, pc: 50, amount: -60 },
+            BasketData { strike: 70, pc: 80, amount: -90 },
+        ];
+        let trade_params = OptionTradeParams {
+            trade_qty: [0; BUCKETS_COUNT],
+            basket: Some(&custom_basket_data),
+            max_cost: Some(500),
+        };
+        let data = DvlInstruction::new::<InstructionOptionTrade>(trade_params).unwrap();
+
+        assert_eq!(data.cmd, Instructions::OptionTrade as u8);
+        assert_eq!(data.version, INSTRUCTION_VERSION);
+
+        assert_eq!(data.basket[0], custom_basket_data[0]);
+        assert_eq!(data.basket[1], custom_basket_data[1]);
+        assert_eq!(data.basket[2], custom_basket_data[2]);
+        assert_eq!(data.basket[3], BasketData { strike: 0, pc: 0, amount: 0 });
+
+        assert_eq!(data.max_cost, 500);
+    }
+
+    #[test]
+    fn test_default_max_cost_instruction_option_trade() {
+        let trade_params = OptionTradeParams {
+            trade_qty: [0; BUCKETS_COUNT],
+            basket: None,
+            max_cost: None,
+        };
+        let data = DvlInstruction::new::<InstructionOptionTrade>(trade_params).unwrap();
+        assert_eq!(data.max_cost, DEFAULT_MAX_COST);
     }
 }
