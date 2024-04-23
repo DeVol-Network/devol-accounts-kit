e9c6712febc68b9667d825072a8378403426d484
Leonid Kabanov <Leonid.Kabanov@devol.network>
2024-04-23 20:09:24 +0200

    Added send transaction

--- src/dvl_client/dvl_client.rs
+++ src/dvl_client/dvl_client.rs
@@ -1,7 +1,13 @@
 use std::error::Error;
 use std::str::FromStr;
 use solana_client::rpc_client::RpcClient;
+use solana_program::instruction::Instruction;
 use solana_program::pubkey::Pubkey;
+use solana_sdk::commitment_config::CommitmentConfig;
+use solana_sdk::compute_budget::ComputeBudgetInstruction;
+use solana_sdk::signature::Keypair;
+use solana_sdk::signer::Signer;
+use solana_sdk::transaction::Transaction;
 use crate::account_readers::dvl_readable::{DvlReadable};
 use crate::accounts::devol_account::DevolAccount;
 use crate::generate_pda::{generate_pda, PDA};
@@ -51,5 +57,54 @@ impl DvlClient {
 
     pub fn account_public_key<'a, T: DvlReadable>(&self, params: T::DvlReadParams<'a>) -> Result<Box<Pubkey>, Box<dyn Error>> {
         T::get_public_key(self, &params)
+    }
+
+    pub fn send_transaction(
+        &self,
+        instruction: Instruction,
+        signer_kp: Keypair,
+        commitment_config: Option<CommitmentConfig>,
+        compute_budget: Option<u32>,
+        max_retries: Option<usize>,
+    ) -> Result<(), Box<dyn Error>> {
+        let mut instructions = Vec::<Instruction>::new();
+        if let Some(max_units) = compute_budget {
+            let compute_budget_instruction =
+                ComputeBudgetInstruction::set_compute_unit_limit(max_units);
+            instructions.push(compute_budget_instruction);
+        }
+        instructions.push(instruction);
+
+        let commitment = commitment_config.unwrap_or_else(|| self.client.commitment());
+
+        let retries = max_retries.unwrap_or(1);
+        for i in 0..retries {
+            let latest_blockhash = self.client.get_latest_blockhash()?;
+
+            let mut new_transaction = Transaction::new_with_payer(&instructions, Some(&signer_kp.pubkey()));
+            new_transaction.try_sign(&[&signer_kp], latest_blockhash)?;
+
+            let send_result = self.client.send_and_confirm_transaction_with_spinner_and_commitment(
+                &new_transaction,
+                commitment,
+            );
+
+            match send_result {
+                Ok(signature) => {
+                    println!("Transaction sent successfully with signature: {}", signature);
+                    return Ok(());
+                },
+                Err(_) if i < retries - 1 => continue,
+                Err(e) => {
+                    eprintln!("Failed to send transaction after {} attempts: {}", retries, e);
+                    return Err(Box::new(e));
+                }
+            }
+        }
+
+        Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "All retry attempts failed",
+        )))
     }
 }
