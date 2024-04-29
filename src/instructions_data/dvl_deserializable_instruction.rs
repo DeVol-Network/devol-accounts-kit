7c23df03b09f92f87b916a3f42d26eec70346880
Niko Melikovi <niko.melikovi@devol.network>
2024-04-29 20:28:06 +0400

    Fixed missing version check

--- src/instructions_data/dvl_deserializable_instruction.rs
+++ src/instructions_data/dvl_deserializable_instruction.rs
@@ -5,7 +5,7 @@ pub trait DvlDeserializableInstruction<'a> {
     fn expected_size() -> usize;
     fn expected_version() -> u8;
     #[inline(always)]
-    fn check_version(vec: &'a[u8]) -> Result<(), DvlError> {
+    fn check_version(vec: &'a [u8]) -> Result<(), DvlError> {
         // Check the second byte for the version number.
         // The first byte usually contains the command identifier,
         // and the second byte consistently holds the version number of the instruction format.
@@ -15,10 +15,11 @@ pub trait DvlDeserializableInstruction<'a> {
         Ok(())
     }
 
-    fn from_vec_le(vec: &'a[u8]) -> Result<&'a Self, DvlError> where Self: Sized {
+    fn from_vec_le(vec: &'a [u8]) -> Result<&'a Self, DvlError> where Self: Sized {
         if vec.len() != Self::expected_size() {
             return Err(DvlError::new(ContractError::InstructionDataLength));
         }
+        Self::expected_version()?;
         Ok(unsafe { &*(vec.as_ptr() as *const Self) })
     }
 }
