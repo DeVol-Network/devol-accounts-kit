bfb78cfb28a5ca1b77a28c52563df0b521609ae1
Leonid Kabanov <Leonid.Kabanov@devol.network>
2024-04-29 13:04:04 +0200

    Added comments

--- src/instructions_data/dvl_deserializable_instruction.rs
+++ src/instructions_data/dvl_deserializable_instruction.rs
@@ -6,6 +6,9 @@ pub trait DvlDeserializableInstruction<'a> {
     fn expected_version() -> u8;
     #[inline(always)]
     fn check_version(vec: &'a[u8]) -> Result<(), DvlError> {
+        // Check the second byte for the version number.
+        // The first byte usually contains the command identifier,
+        // and the second byte consistently holds the version number of the instruction format.
         if vec[1] != Self::expected_version() {
             return Err(DvlError::new(ContractError::InstructionDataVersion));
         }
