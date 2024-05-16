141b44f232cb4015d091b3274ccc0ed2829b9a96
Leonid Kabanov <Leonid.Kabanov@devol.network>
2024-05-16 13:49:34 +0200

    Debug+Display realization for WorkerState

--- src/accounts/worker/worker_state.rs
+++ src/accounts/worker/worker_state.rs
@@ -1,10 +1,17 @@
+use std::fmt;
 use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, PartialOrd, Copy, Clone, Serialize, Deserialize)]
+#[derive(PartialEq, PartialOrd, Copy, Clone, Debug, Serialize, Deserialize)]
 #[repr(u32)]
 pub enum WorkerState {
     Unassigned = 1,
     Assigned = 2,
     Inactive = 3,
     Active = 4,
+}
+
+impl fmt::Display for WorkerState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", format!("{:?}", self).to_lowercase())
+    }
 }
